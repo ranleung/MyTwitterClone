4fb767720abf9f1aa9cee6cfa0288a6bab1692e6
Randall Leung <leungran@gmail.com>
2014-10-06 13:34:04 -0700

    initial commit

---
+++ MyTwitterClone/HomeTimeLineViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeTimeLineViewController.swift
+//  MyTwitterClone
+//
+//  Created by Randall Leung on 10/6/14.
+//  Copyright (c) 2014 Randall. All rights reserved.
+//
+
+import UIKit
+
+class HomeTimeLineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
