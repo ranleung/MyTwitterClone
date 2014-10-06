61e237396ae3808b152255da512f567a5b001c1a
Randall Leung <leungran@gmail.com>
2014-10-06 15:23:42 -0700

    UITableViewDataSource linked up

--- MyTwitterClone/HomeTimeLineViewController.swift
+++ MyTwitterClone/HomeTimeLineViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class HomeTimeLineViewController: UIViewController {
+class HomeTimeLineViewController: UIViewController, UITableViewDataSource {
     
-
+    @IBOutlet weak var TableView: UITableView!
     var tweets : [Tweet]?
 
     override func viewDidLoad() {
@@ -25,6 +25,25 @@ class HomeTimeLineViewController: UIViewController {
         
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.tweets != nil {
+            return self.tweets!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Step 1: dequeue the cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("TWEET_CELL", forIndexPath: indexPath) as UITableViewCell
+        // Step 2 figure out which model object youre going to use to configure the cell
+        // This is where we would grab a reference to the correct tweet and use it to configure the cell
+        let tweet = self.tweets?[indexPath.row]
+        cell.textLabel?.text = tweet?.text
+        // Step 3, return the cell
+        return cell
+        
+    }
     
 
 }
